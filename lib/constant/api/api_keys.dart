339962df3ccfb718e039e3ea2fa7226983be43f2
Maharramli_Ziya <113660361+MaharramovZiya@users.noreply.github.com>
2024-06-06 23:09:36 +0400

    Update api_keys.dart

--- lib/constant/api/api_keys.dart
+++ lib/constant/api/api_keys.dart
@@ -1,5 +1,5 @@
 class APIConstants {
-  static const api_key = '81d2cfe9c78f0d9e46febcc6ff2acbd4';
-  static const top_rated_api_key = '81d2cfe9c78f0d9e46febcc6ff2acbd4';
+  static const api_key = '';
+  static const top_rated_api_key = '';
   static const image_key = 'https://image.tmdb.org/t/p/w500/';
 }
